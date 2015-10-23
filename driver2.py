612d2399e400a19f87e68d1f6148c1c23d899fac
Eric Roden <roden026@umn.edu>
2015-10-23 11:45:41 -0500

    Made actual 3D plots again and set up side by side for stereoscopic use.

--- driver2.py
+++ driver2.py
@@ -12,63 +12,94 @@ import matplotlib
 import six
 import dateutil
 import itertools
+from mpl_toolkits.mplot3d import Axes3D
 
 usage_mesg = 'driver2.py <csv file(s)>'
 
+'''
+**********************************************************************************************
+Note that using system input needs to be added back in once testing in pycharm is completed
+**********************************************************************************************
+'''
+
 # Checks if files are available.
-filename_csv = sys.argv[1]
-if( not os.access(filename_csv,os.R_OK) ):
-    print "%s is not accessible."%filename_csv
+filename_csv = 'example.csv'  # sys.argv[1]
+if not os.access(filename_csv, os.R_OK):
+    print "%s is not accessible." % filename_csv
     print usage_mesg
     sys.exit(1)
 
 # Checks to see if you want to test pos and neg or just one or the other
-#***** see if this is needed ****
-if(len(sys.argv) == 2 ):
-    filename_csv = sys.argv[1]
-    elementalList = extractNeededElementalData(filename_csv)
-    ratiosList = processElementalData(elementalList)
-    #Note, at this point the elementalList and ratiosList will not be aligned
-    #because the processing of the ratios removes lines where not all 3 elements
-    #were present (rare occurance but may happen)
+# ***** see if this is needed ****
+# if (len(sys.argv) == 2):
+    # filename_csv = sys.argv[1]
+elementalList = extractNeededElementalData(filename_csv)
+ratiosList = processElementalData(elementalList)
+    # Note, at this point the elementalList and ratiosList will not be aligned
+    # because the processing of the ratios removes lines where not all 3 elements
+    # were present (rare occurance but may happen)
 
 # Creates the actual graph itself.	
 
 print "Displaying plot"
-	
+
 # Graphs the data provided and labels axes
 
 area = 10.0
 
-fig = plt.figure()
+fig = plt.figure(figsize=(17, 7))
 fig.suptitle('Van Krevelen Diagram - Nitrogen Check', fontsize=14, fontweight='bold')
-ax = fig.add_subplot(111)
-fig.subplots_adjust(top=0.85)
+ax = fig.add_subplot(121, projection='3d')
 
 ax.set_xlabel('O:C Ratio')
 ax.set_ylabel('H:C Ratio')
+ax.set_zlabel('N:C Ratio')
 
 # Creates a list for plotting purposes where two elements are lists of compounds with N and without respectively
-listByN = [[],[]]
+listByN = [[], []]
 withN = None
 withoutN = None
 for i in range(len(ratiosList[2])):
     if ratiosList[2][i]:
-        listByN[0].append([ratiosList[1][i],ratiosList[0][i], 'r', '^'])
+        listByN[0].append([ratiosList[1][i], ratiosList[0][i], ratiosList[3][i], 'r', '^'])
     else:
-        listByN[1].append([ratiosList[1][i],ratiosList[0][i], 'b', 'o'])
+        listByN[1].append([ratiosList[1][i], ratiosList[0][i], ratiosList[3][i], 'b', 'o'])
 
 counter = 0
 for i in listByN:
     for j in i:
         if counter == 0:
-           withN = plt.scatter(j[0], j[1], 15.0, j[2], j[3], alpha = .25)
+            withN = ax.scatter(xs=j[0], ys=j[1], zs=j[2], s=15.0, c=j[3], marker=j[4], alpha=.25)
         else:
-            withoutN = plt.scatter(j[0], j[1], 15.0, j[2], j[3], alpha = .25)
+            withoutN = ax.scatter(xs=j[0], ys=j[1], zs=j[2], s=15.0, c=j[3], marker=j[4], alpha=.25)
     counter += 1
 
+plt.legend((withN, withoutN), ('Does have N', 'Does not have N'), scatterpoints=1, loc='lower left', ncol=1, fontsize=9)
 
-plt.legend((withN, withoutN), ('Does have N', 'Does not have N'), scatterpoints = 1, loc='lower left', ncol=1,fontsize = 9)
-plt.show()
+'''
+Second plot. Probably a more efficient way of doing this so will rework once testing is done
+'''
+ax2 = fig.add_subplot(122, projection='3d')
+
+ax2.set_xlabel('O:C Ratio')
+ax2.set_ylabel('H:C Ratio')
+ax2.set_zlabel('N:C Ratio')
+
+counter = 0
+for i in listByN:
+    for j in i:
+        if counter == 0:
+            withN = ax2.scatter(xs=j[0], ys=j[1], zs=j[2], s=15.0, c=j[3], marker=j[4], alpha=.25)
+        else:
+            withoutN = ax2.scatter(xs=j[0], ys=j[1], zs=j[2], s=15.0, c=j[3], marker=j[4], alpha=.25)
+    counter += 1
+
+ax2.azim -= 6
+
+plt.legend((withN, withoutN), ('Does have N', 'Does not have N'), scatterpoints=1, loc='lower left', ncol=1, fontsize=9)
+# END SECOND PLOT CODE
+
+
+plt.savefig("Example2.pdf")
 
 print("done")
--- processElementalData.py
+++ processElementalData.py
@@ -1,7 +1,7 @@
 # takes in list of values for C, H, and O and then computes the needed ratios
 def processElementalData(elementalList):
-    # 0 = H:C, 1 = O:C, 2 = Present/Not (True, False)
-    ratiosList = [[],[],[]]
+    # 0 = H:C, 1 = O:C, 2 = Present/Not (True, False), 3: N:C
+    ratiosList = [[], [], [], []]
     
     #Computes all the ratios and adds them to the ratiosList
     for line in elementalList:
@@ -15,6 +15,8 @@ def processElementalData(elementalList):
                 ratiosList[2].append(False)
             else:
                 ratiosList[2].append(True)
+            NtoC = line[3]/line[0]
+            ratiosList[3].append(NtoC)
        			
     return ratiosList
 
